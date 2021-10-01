f87a48c086a362e7e644e616bc0ad4ec466c9fae
Alexander Terekhov <swimalext@gmail.com>
2021-10-01 13:03:57 +0300

    fix: scripts and styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,55 @@
+//Находим popup
 const modalWindow = document.querySelector('.popup');
+//В popup находим кнопку закрытия
 const modalWindowCloseButton = modalWindow.querySelector('.popup__close');
+//Находим секцию profile
 const profile = document.querySelector('.profile');
+//Находим в profile кнопку редактирования
 const editButton = profile.querySelector('.profile__edit-button');
+//Находим форму в popup
+const popupForm = modalWindow.querySelector('.popup__content');
+//Находим поля формы
+const userName = popupForm.querySelector('.popup__user_name');
+const userProfile = popupForm.querySelector('.popup__user_profile');
+
+
+//Выбираем элементы, куда добавятся значения полей
 const putName = profile.querySelector('.profile__title');
 const putProfile = profile.querySelector('.profile__subtitle');
 
-const popupContent = modalWindow.querySelector('.popup__content');
-const userName = popupContent.querySelector('.popup__user_name');
-const userProfile = popupContent.querySelector('.popup__user_profile');
-const popupSave = modalWindow.querySelector('.popup__save');
-
-
+//Для отображения popup
 function modalWindowOpened() {
     modalWindow.classList.add('popup_is-opened');
-}
-
+};
+//Для скрытия popup
 function modalWindowClosed() {
     modalWindow.classList.remove('popup_is-opened');
-}
+};
 
 function reloadContent() {
     userName.value = putName.textContent;
     userProfile.value = putProfile.textContent;
-}
+    //При открытии popup поля заполняются данными из profile
+    modalWindowOpened()
+};
 
-
-
+//Для отправки формы
 function submitContent(evt) {
     evt.preventDefault();
-    const nameInput = userName.value;
-    const profileInput = userProfile.value;
-    putName.textContent = nameInput;
-    putProfile.textContent = profileInput;
-}
+    //Получение полей input из свойства Value
+    let userNameValue = userName.value;
+    let userProfileValue = userProfile.value;
+    //Добавляем значения
+    putName.textContent = userNameValue;
+    putProfile.textContent = userProfileValue;
+    //Добавляем закрытие после отправки
+    modalWindowClosed()
+};
 
-popupSave.addEventListener('click', submitContent);
+//Следим за событием 'submit'
+popupForm.addEventListener('submit', submitContent);
 
-editButton.addEventListener('click', modalWindowOpened);
 editButton.addEventListener('click', reloadContent);
 
+//Для закрытия popup по кнопке "Закрыть"
 modalWindowCloseButton.addEventListener('click', modalWindowClosed);
