fb9ea25a25035760a77dc26d76fff42db7c72b02
Alexander Terekhov <swimalext@gmail.com>
2021-11-21 23:10:56 +0300

    fix: filenames

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { Card } from "./card.js";
-import { FormValidator } from "./formValidator.js";
+import { Card } from "./Card.js";
+import { FormValidator } from "./FormValidator.js";
 
 //Находим popup profile
 const popupProfile = document.querySelector('.popup_item_create-profile');
